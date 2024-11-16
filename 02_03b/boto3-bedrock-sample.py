0e996c1fefdba166c5587fa3c6261a9a931ff9e8
jflynn99 <jflynn1492@gmail.com>
2024-11-16 16:56:01 +0000

    Initial commit

--- 02_03b/boto3-bedrock-sample.py
+++ 02_03b/boto3-bedrock-sample.py
@@ -1,13 +1,28 @@
 #Imports
-
+import boto3
+import json
 #Create the bedrock client
-
+bedrock = boto3.client('bedrock-runtime')
 #Setting the prompt
-
+propmt_data = """Command: Write me a blog about product managers building things themselves on AWS bedrock.
+Blog:
+"""
 #Model specification
-
+modelId = "amazon.titan-text-express-v1"
+accept = "application/json"
+contentType = "application/json"
 #Configuring parameters to invoke the model
-
+body = json.dumps({
+  "inputText" : propmt_data,
+  "testGenerationConfig" : {
+  "maxTokenCount" : 1000
+    }
+  })
 #Invoke the model
-
+response = bedrock.invoke_model(
+  body = body, modelId = modelId, accept = accept, contentType = contentType
+)
 #Parsing and displaying the output
+response_body = json.loads(response.get('body').read())
+output = response_body.get('resuts')[0].get("outputText")
+print(output)
